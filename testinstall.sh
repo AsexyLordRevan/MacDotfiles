577d022fe4eb67f24448469fdad00ce8a8e964cd
AsexyLordRevan <155589214+AsexyLordRevan@users.noreply.github.com>
2024-10-09 13:49:54 -0700

    Update testinstall.sh

--- testinstall.sh
+++ testinstall.sh
@@ -3,11 +3,15 @@
 set -e
 
 yes | brew install koekeishiya/formulae/yabai
-yes | brew install koekeishiya/formulae/skhd
-if [-d "~/yabai"]; then
+if [-d "~/.config/yabai"]; then
     cp -r ~/.config/yabai ~/.config/yabai_backup
     rm -rf ~/yabai
 fi
+yes | brew install koekeishiya/formulae/skhd
 git clone https://github.com/asexylordrevan/yabai ~/yabai
-
+if [-d "~/.config/skhd"]; then
+    cp -r ~/.config/skhd ~/.config/skhd_backup
+    rm -rf ~/skhd
+fi
+git clone https://github.com/asexylordrevan/skhd ~/skhd
 skhd --start-service
